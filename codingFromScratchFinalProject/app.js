b47123127e13e3fd875db236cb768d12e103dffb
filibertocarnal <93798390+filibertocarnal@users.noreply.github.com>
2021-11-05 19:39:15 -0700

    Add files via upload

---
+++ codingFromScratchFinalProject/app.js
@@ -0,0 +1,16 @@
+function idCard() {
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var address = document.getElementById('addressId').value;
+  
+ document.getElementById('postFullName').innerHTML = firstName + " " + lastName;
+document.getElementById("postAddress").innerHTML = address;
+  
+var age = parseInt(document.getElementById('ageId').value);
+  
+var phoneNumber =
+parseInt(document.getElementById('phoneNumber').value);
+ 
+document.getElementById('postPhoneNumber').innerHTML = "Phone Number: " + phoneNumber;
+document.getElementById('postAge').innerHTML = "Age: " + age;
+}
